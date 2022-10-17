4a0c93b52b41e5965c3c6883fdc40214ef3ad254
kevinization <kevinrodarte23@gmail.com>
2022-10-17 16:11:29 -0500

    Added a comment

--- src/main/java/net/kevin/mccourse/MCCourseMod.java
+++ src/main/java/net/kevin/mccourse/MCCourseMod.java
@@ -9,6 +9,7 @@ public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// A comment
 	@Override
 	public void onInitialize() {
 
